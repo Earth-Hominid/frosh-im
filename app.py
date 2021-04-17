8863730bccd8adbe9dfe3806f6e8837938c9e1ad
C <earthhominid@gmail.com>
2021-04-17 12:37:53 -0300

    switched name to email,sql deleted, email functionality added

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+import os
+
+form cs50 
 from flask import Flask, redirect, render_template, request
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+mail = Mail(app)
 
 REGISTRANTS = {}
 
@@ -19,19 +30,16 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
-    name = request.form.get("name")
-    if not name:
-        return render_template("error.html", message="Missing name")
+    email = request.form.get("email")
+    if not email:
+        return render_template("error.html", message="Missing email")
     sport = request.form.get("sport")
     if not sport:
         render_template("error.html", message="Missing sport")
     if sport not in SPORTS:
         return render_template("error.html", message="Invalid sport")
+
+    message = Message("You are registered!", recipients=[email])
+    mail.send(message)
     
-    REGISTRANTS[name] = sport
-
-    return redirect("/registrants")
-
-@app.route("/registrants")
-def registrants():
-    return render_template("registrants.html", registrants=REGISTRANTS)
+    return render_template("success.html") 
